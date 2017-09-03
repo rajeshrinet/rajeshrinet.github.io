6b870ccc0f85317df16294a0f2c2c4f79c9306fa
Rajesh Singh <rajeshrinet@gmail.com>
2017-09-03 13:47:29 +0530

    update the theme of blog

--- blog/blog-generator/site-generator.py
+++ blog/blog-generator/site-generator.py
@@ -10,19 +10,17 @@ import numpy as np
 A0 = sys.argv[1] # index.html file
 A1 = sys.argv[2] # first few lines of the template
 A2 = sys.argv[3] # html of ipython notebook file
-A3 = sys.argv[4] # last few lines for footer
 
 f1 = open( '%s'%(A1), 'r')   # read from the first file
 f2 = open( '%s'%(A2), 'r')   # read from the second file
-f3 = open( '%s'%(A3), 'r')   # read from the third file
 
 template    = f1.readlines()
 lines_write = f2.readlines()
-side_bar    = f3.readlines()
+#side_bar    = f3.readlines()
 
 
 f0 = open( '%s'%(A0), 'w')   # open the index.html to be written
 f0.writelines(template)
 f0.writelines(lines_write)
-f0.writelines(side_bar)
+#f0.writelines(side_bar)
 f0.close()
