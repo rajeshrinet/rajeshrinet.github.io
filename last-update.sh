7dc834faba8209adc14e24e2444e0957bf63cdce
Rajesh Singh <rajeshrinet@gmail.com>
2021-10-04 04:50:57 +0530

    minor

--- last-update.sh
+++ last-update.sh
@@ -2,20 +2,20 @@
 
 file='index.html'
 echo "Updating file: $file" 
-sed -i "" "s/.*last updated.*/last updated $(date '+%F')/g" $file
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 
 
 file='publications/index.html'
 echo "Updating file: $file" 
-sed -i "" "s/.*last updated.*/last updated $(date '+%F')/g" $file
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 
 
 
 file='contact/index.html'
 echo "Updating file: $file" 
-sed -i "" "s/.*last updated.*/last updated $(date '+%F')/g" $file
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 
 
 file='research/index.html'
 echo "Updating file: $file" 
-sed -i "" "s/.*last updated.*/last updated $(date '+%F')/g" $file
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
