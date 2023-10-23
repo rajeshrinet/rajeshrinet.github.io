4c8a49f4d8ba8b51c7064b631b9fcebc9a320a35
Rajesh Singh <rajeshrinet@gmail.com>
2023-10-23 20:19:54 +0530

    add a teaching page

--- last-update.sh
+++ last-update.sh
@@ -13,3 +13,8 @@ sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 file='research/index.html'
 echo "Updating file: $file" 
 sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
+
+
+file='teaching/index.html'
+echo "Updating file: $file" 
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
