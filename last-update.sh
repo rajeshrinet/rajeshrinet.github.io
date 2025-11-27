39453c5d29b29b4fa708c039f61ae325cf6162f0
Rajesh Singh <rajeshrinet@gmail.com>
2025-11-28 04:18:10 +0530

    add new file

--- last-update.sh
+++ last-update.sh
@@ -4,6 +4,9 @@ file='index.html'
 echo "Updating file: $file" 
 sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 
+file='group/index.html'
+echo "Updating file: $file" 
+sed -i "" "s/.*last updated.*/last updated $(date '+%v')/g" $file
 
 file='publications/index.html'
 echo "Updating file: $file" 
